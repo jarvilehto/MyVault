ce43e3001c046513c3091e0564b6e280bcfe9229
J. <77263040+jarvilehto@users.noreply.github.com>
2025-02-02 20:49:24 +0200

    Next.js boilerplate

---
+++ myvault/src/app/login/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
+export default function Login() {
+  return (
+    <div className={styles.page}>
+      <h1>Login</h1>
+    </div>
+  );
+}
