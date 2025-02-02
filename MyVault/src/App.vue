27319e902912d9c9657ba3ed4673c0a9d01a2082
J. <77263040+jarvilehto@users.noreply.github.com>
2025-02-02 19:10:34 +0200

    WIP: Login / Register

--- MyVault/src/App.vue
+++ MyVault/src/App.vue
@@ -1,18 +1,16 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import MyVault from "./components/MyVault.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+      <MyVault />
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
       </nav>
     </div>
   </header>
---
+++ MyVault/src/views/Login.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="login">
+    <h1>login</h1>
+  </main>
+</template>
